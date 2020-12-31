c64c90c3a9cf1efc19eebd040d9d7486d6428a48
824750130 <e824750130@qq.com>
2020-12-31 11:23:19 +0800

    support python2

---
+++ python2/alibabacloud_tea_openapi/models.py
@@ -0,0 +1,265 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from Tea.model import TeaModel
+from Tea.converter import TeaConverter
+
+from alibabacloud_credentials.client import Client as CredentialClient
+
+
+class Config(TeaModel):
+    """
+    Model for initing client
+    """
+    def __init__(self, access_key_id=None, access_key_secret=None, security_token=None, protocol=None,
+                 region_id=None, read_timeout=None, connect_timeout=None, http_proxy=None, https_proxy=None, credential=None,
+                 endpoint=None, no_proxy=None, max_idle_conns=None, network=None, user_agent=None, suffix=None,
+                 socks_5proxy=None, socks_5net_work=None, endpoint_type=None, open_platform_endpoint=None, type=None,
+                 signature_algorithm=None):
+        # accesskey id
+        self.access_key_id = TeaConverter.to_unicode(access_key_id)  # type: unicode
+        # accesskey secret
+        self.access_key_secret = TeaConverter.to_unicode(access_key_secret)  # type: unicode
+        # security token
+        self.security_token = TeaConverter.to_unicode(security_token)  # type: unicode
+        # http protocol
+        self.protocol = TeaConverter.to_unicode(protocol)  # type: unicode
+        # region id
+        self.region_id = TeaConverter.to_unicode(region_id)  # type: unicode
+        # read timeout
+        self.read_timeout = read_timeout  # type: int
+        # connect timeout
+        self.connect_timeout = connect_timeout  # type: int
+        # http proxy
+        self.http_proxy = TeaConverter.to_unicode(http_proxy)  # type: unicode
+        # https proxy
+        self.https_proxy = TeaConverter.to_unicode(https_proxy)  # type: unicode
+        # credential
+        self.credential = credential  # type: CredentialClient
+        # endpoint
+        self.endpoint = TeaConverter.to_unicode(endpoint)  # type: unicode
+        # proxy white list
+        self.no_proxy = TeaConverter.to_unicode(no_proxy)  # type: unicode
+        # max idle conns
+        self.max_idle_conns = max_idle_conns  # type: int
+        # network for endpoint
+        self.network = TeaConverter.to_unicode(network)  # type: unicode
+        # user agent
+        self.user_agent = TeaConverter.to_unicode(user_agent)  # type: unicode
+        # suffix for endpoint
+        self.suffix = TeaConverter.to_unicode(suffix)  # type: unicode
+        # socks5 proxy
+        self.socks_5proxy = TeaConverter.to_unicode(socks_5proxy)  # type: unicode
+        # socks5 network
+        self.socks_5net_work = TeaConverter.to_unicode(socks_5net_work)  # type: unicode
+        # endpoint type
+        self.endpoint_type = TeaConverter.to_unicode(endpoint_type)  # type: unicode
+        # OpenPlatform endpoint
+        self.open_platform_endpoint = TeaConverter.to_unicode(open_platform_endpoint)  # type: unicode
+        # credential type
+        self.type = TeaConverter.to_unicode(type)  # type: unicode
+        # Signature Algorithm
+        self.signature_algorithm = TeaConverter.to_unicode(signature_algorithm)  # type: unicode
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        result = dict()
+        if self.access_key_id is not None:
+            result['accessKeyId'] = self.access_key_id
+        if self.access_key_secret is not None:
+            result['accessKeySecret'] = self.access_key_secret
+        if self.security_token is not None:
+            result['securityToken'] = self.security_token
+        if self.protocol is not None:
+            result['protocol'] = self.protocol
+        if self.region_id is not None:
+            result['regionId'] = self.region_id
+        if self.read_timeout is not None:
+            result['readTimeout'] = self.read_timeout
+        if self.connect_timeout is not None:
+            result['connectTimeout'] = self.connect_timeout
+        if self.http_proxy is not None:
+            result['httpProxy'] = self.http_proxy
+        if self.https_proxy is not None:
+            result['httpsProxy'] = self.https_proxy
+        if self.credential is not None:
+            result['credential'] = self.credential
+        if self.endpoint is not None:
+            result['endpoint'] = self.endpoint
+        if self.no_proxy is not None:
+            result['noProxy'] = self.no_proxy
+        if self.max_idle_conns is not None:
+            result['maxIdleConns'] = self.max_idle_conns
+        if self.network is not None:
+            result['network'] = self.network
+        if self.user_agent is not None:
+            result['userAgent'] = self.user_agent
+        if self.suffix is not None:
+            result['suffix'] = self.suffix
+        if self.socks_5proxy is not None:
+            result['socks5Proxy'] = self.socks_5proxy
+        if self.socks_5net_work is not None:
+            result['socks5NetWork'] = self.socks_5net_work
+        if self.endpoint_type is not None:
+            result['endpointType'] = self.endpoint_type
+        if self.open_platform_endpoint is not None:
+            result['openPlatformEndpoint'] = self.open_platform_endpoint
+        if self.type is not None:
+            result['type'] = self.type
+        if self.signature_algorithm is not None:
+            result['signatureAlgorithm'] = self.signature_algorithm
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('accessKeyId') is not None:
+            self.access_key_id = m.get('accessKeyId')
+        if m.get('accessKeySecret') is not None:
+            self.access_key_secret = m.get('accessKeySecret')
+        if m.get('securityToken') is not None:
+            self.security_token = m.get('securityToken')
+        if m.get('protocol') is not None:
+            self.protocol = m.get('protocol')
+        if m.get('regionId') is not None:
+            self.region_id = m.get('regionId')
+        if m.get('readTimeout') is not None:
+            self.read_timeout = m.get('readTimeout')
+        if m.get('connectTimeout') is not None:
+            self.connect_timeout = m.get('connectTimeout')
+        if m.get('httpProxy') is not None:
+            self.http_proxy = m.get('httpProxy')
+        if m.get('httpsProxy') is not None:
+            self.https_proxy = m.get('httpsProxy')
+        if m.get('credential') is not None:
+            self.credential = m.get('credential')
+        if m.get('endpoint') is not None:
+            self.endpoint = m.get('endpoint')
+        if m.get('noProxy') is not None:
+            self.no_proxy = m.get('noProxy')
+        if m.get('maxIdleConns') is not None:
+            self.max_idle_conns = m.get('maxIdleConns')
+        if m.get('network') is not None:
+            self.network = m.get('network')
+        if m.get('userAgent') is not None:
+            self.user_agent = m.get('userAgent')
+        if m.get('suffix') is not None:
+            self.suffix = m.get('suffix')
+        if m.get('socks5Proxy') is not None:
+            self.socks_5proxy = m.get('socks5Proxy')
+        if m.get('socks5NetWork') is not None:
+            self.socks_5net_work = m.get('socks5NetWork')
+        if m.get('endpointType') is not None:
+            self.endpoint_type = m.get('endpointType')
+        if m.get('openPlatformEndpoint') is not None:
+            self.open_platform_endpoint = m.get('openPlatformEndpoint')
+        if m.get('type') is not None:
+            self.type = m.get('type')
+        if m.get('signatureAlgorithm') is not None:
+            self.signature_algorithm = m.get('signatureAlgorithm')
+        return self
+
+
+class OpenApiRequest(TeaModel):
+    def __init__(self, headers=None, query=None, body=None, stream=None):
+        self.headers = headers  # type: dict[unicode, unicode]
+        self.query = query  # type: dict[unicode, unicode]
+        self.body = body  # type: any
+        self.stream = stream  # type: READABLE
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.query is not None:
+            result['query'] = self.query
+        if self.body is not None:
+            result['body'] = self.body
+        if self.stream is not None:
+            result['stream'] = self.stream
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('query') is not None:
+            self.query = m.get('query')
+        if m.get('body') is not None:
+            self.body = m.get('body')
+        if m.get('stream') is not None:
+            self.stream = m.get('stream')
+        return self
+
+
+class Params(TeaModel):
+    def __init__(self, action=None, version=None, protocol=None, pathname=None, method=None, auth_type=None,
+                 body_type=None, req_body_type=None, style=None):
+        self.action = TeaConverter.to_unicode(action)  # type: unicode
+        self.version = TeaConverter.to_unicode(version)  # type: unicode
+        self.protocol = TeaConverter.to_unicode(protocol)  # type: unicode
+        self.pathname = TeaConverter.to_unicode(pathname)  # type: unicode
+        self.method = TeaConverter.to_unicode(method)  # type: unicode
+        self.auth_type = TeaConverter.to_unicode(auth_type)  # type: unicode
+        self.body_type = TeaConverter.to_unicode(body_type)  # type: unicode
+        self.req_body_type = TeaConverter.to_unicode(req_body_type)  # type: unicode
+        self.style = TeaConverter.to_unicode(style)  # type: unicode
+
+    def validate(self):
+        self.validate_required(self.action, 'action')
+        self.validate_required(self.version, 'version')
+        self.validate_required(self.protocol, 'protocol')
+        self.validate_required(self.pathname, 'pathname')
+        self.validate_required(self.method, 'method')
+        self.validate_required(self.auth_type, 'auth_type')
+        self.validate_required(self.body_type, 'body_type')
+        self.validate_required(self.req_body_type, 'req_body_type')
+
+    def to_map(self):
+        result = dict()
+        if self.action is not None:
+            result['action'] = self.action
+        if self.version is not None:
+            result['version'] = self.version
+        if self.protocol is not None:
+            result['protocol'] = self.protocol
+        if self.pathname is not None:
+            result['pathname'] = self.pathname
+        if self.method is not None:
+            result['method'] = self.method
+        if self.auth_type is not None:
+            result['authType'] = self.auth_type
+        if self.body_type is not None:
+            result['bodyType'] = self.body_type
+        if self.req_body_type is not None:
+            result['reqBodyType'] = self.req_body_type
+        if self.style is not None:
+            result['style'] = self.style
+        return result
+
+    def from_map(self, m=None):
+        m = m or dict()
+        if m.get('action') is not None:
+            self.action = m.get('action')
+        if m.get('version') is not None:
+            self.version = m.get('version')
+        if m.get('protocol') is not None:
+            self.protocol = m.get('protocol')
+        if m.get('pathname') is not None:
+            self.pathname = m.get('pathname')
+        if m.get('method') is not None:
+            self.method = m.get('method')
+        if m.get('authType') is not None:
+            self.auth_type = m.get('authType')
+        if m.get('bodyType') is not None:
+            self.body_type = m.get('bodyType')
+        if m.get('reqBodyType') is not None:
+            self.req_body_type = m.get('reqBodyType')
+        if m.get('style') is not None:
+            self.style = m.get('style')
+        return self
+
+
