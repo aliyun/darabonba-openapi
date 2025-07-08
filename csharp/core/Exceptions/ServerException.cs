f650ba0022fd7d277776ec08c15a8a2e85e3bd82
PanPanZou <865287328@qq.com>
2025-07-08 16:03:27 +0800

    fix: add params.validate

--- csharp/core/Exceptions/ServerException.cs
+++ csharp/core/Exceptions/ServerException.cs
@@ -1,11 +1,15 @@
+// This file is auto-generated, don't edit it. Thanks.
+
 using System;
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+using Darabonba.Utils;
 
 namespace AlibabaCloud.OpenApiClient.Exceptions
 {
-    public class ServerException : AlibabaCloudException {
+    public class ServerException : AlibabaCloudException
+    {
 
     }
 
